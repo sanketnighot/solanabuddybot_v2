7200cdf27fe830ce3931d226d777a16b928d10f7
sanketnighot <sanketnighot25@gmail.com>
2024-08-09 11:13:39 +0530

    init: setting up eslint, prettier, husky, lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,35 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
+import eslint from "@eslint/js"
+import eslintPluginPrettier from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
 
 export default [
   { files: ["src/**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  { ignores: ["node_modules/**/*", "dist/**/*"] },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  { ignores: ["node_modules/**", "dist/**"] },
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      prettier: eslintPluginPrettier,
+    },
+  },
+  {
+    rules: {
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  eslint.configs.recommended,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ]
