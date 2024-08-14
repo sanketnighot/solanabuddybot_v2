5795dc00c1475c8c582e294d1c4bdc1410ab719d
sanketnighot <sanketnighot25@gmail.com>
2024-08-14 20:12:17 +0530

    fix: removed the password creation method

--- src/services/TelegramBot/mainMenu/manageAccount.ts
+++ src/services/TelegramBot/mainMenu/manageAccount.ts
@@ -11,8 +11,7 @@ import bot from ".."
 export const manageAccount = async (query: CallbackQuery) => {
   try {
     //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, query_action, query_info, query_status, password, del] =
-      query.data!.split("/")
+    const [_, query_action, query_info, query_status] = query.data!.split("/")
     switch (query_info) {
       case "seed":
         if (query_action === "create") {
@@ -32,127 +31,9 @@ export const manageAccount = async (query: CallbackQuery) => {
       case "keypair":
         if (query_status === "c") {
           if (query_action === "create") {
-            if (
-              password === undefined ||
-              (password && password.length < 6) ||
-              del === "del"
-            ) {
-              if (del === "del") {
-                await bot.deleteMessage(
-                  query.message?.chat.id || 0,
-                  query.message?.message_id || 0
-                )
-                return
-              }
-              const inline_keyboard = [
-                [
-                  {
-                    text: "1️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/1"
-                        : "account/create/keypair/c/" + password + "1",
-                  },
-                  {
-                    text: "2️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/2"
-                        : "account/create/keypair/c/" + password + "2",
-                  },
-                  {
-                    text: "3️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/3"
-                        : "account/create/keypair/c/" + password + "3",
-                  },
-                ],
-                [
-                  {
-                    text: "4️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/4"
-                        : "account/create/keypair/c/" + password + "4",
-                  },
-                  {
-                    text: "5️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/5"
-                        : "account/create/keypair/c/" + password + "5",
-                  },
-                  {
-                    text: "6️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/6"
-                        : "account/create/keypair/c/" + password + "6",
-                  },
-                ],
-                [
-                  {
-                    text: "7️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/7"
-                        : "account/create/keypair/c/" + password + "7",
-                  },
-                  {
-                    text: "8️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/8"
-                        : "account/create/keypair/c/" + password + "8",
-                  },
-                  {
-                    text: "9️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/9"
-                        : "account/create/keypair/c/" + password + "9",
-                  },
-                ],
-                [
-                  {
-                    text: "🔄",
-                    callback_data: "account/create/keypair/c/",
-                  },
-                  {
-                    text: "0️⃣",
-                    callback_data:
-                      password === undefined
-                        ? "account/create/keypair/c/0"
-                        : "account/create/keypair/c/" + password + "0",
-                  },
-                  {
-                    text: "❌",
-                    callback_data: "account/create/keypair/c//del",
-                  },
-                ],
-              ]
-              let pinMessage = "Enter Password for this Account\n\n"
-              pinMessage +=
-                password === undefined
-                  ? "⭕️".repeat(6)
-                  : "⛔️".repeat(password.length) +
-                    "⭕️".repeat(6 - password.length)
-              await bot.deleteMessage(
-                query.message?.chat.id || 0,
-                query.message?.message_id || 0
-              )
-              await SendBotResponse(query.message?.chat.id, pinMessage, {
-                reply_markup: {
-                  inline_keyboard,
-                },
-              })
-              return
-            }
-            console.log({ password })
             const createAccountResponse = await createAccountUsingKeypair(
               BigInt(query.message?.chat.id || 0),
-              password
+              "password"
             )
             if (!createAccountResponse.success) {
               await SendBotResponse(
